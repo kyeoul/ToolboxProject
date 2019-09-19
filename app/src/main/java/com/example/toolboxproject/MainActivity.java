7a46d8da3dca403ff35736a32e98abbf5e528f3b
kyeoul <54390304+kyeoul@users.noreply.github.com>
2019-09-18 21:04:26 -0500

    yes i committed

--- app/src/main/java/com/example/toolboxproject/MainActivity.java
+++ app/src/main/java/com/example/toolboxproject/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.toolboxproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
@@ -16,9 +19,13 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private boolean isCreative = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +57,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> arg0, View v, int position, long id){
         ImageView image = (ImageView) findViewById(R.id.imageView) ;
-        CharSequence string = "wwowowowoowow it worked";
-        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
         Resources res = getResources();
         String[] themeArray = res.getStringArray(R.array.spinnerChoices);
         if(themeArray[position].equals("Summer")){
@@ -112,6 +117,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     textView.setTextColor((getResources().getColor(R.color.colorBlack)));
                 }
                 break;
+        }
+    }
+
+    public void onClick(View v){
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        TextView surpriseText = (TextView) findViewById(R.id.creativeText);
+        if(!isCreative) {
+            Toast.makeText(this, "plz rate 5 stars on apple store", Toast.LENGTH_SHORT).show();
+            image.setImageResource(R.drawable.doge);
+            surpriseText.setVisibility(View.VISIBLE);
+            isCreative = true;
+        }
+        else{
+            image.setImageResource(R.drawable.whole_brain);
+            surpriseText.setVisibility(View.INVISIBLE);
+            isCreative = false;
         }
     }
 
